8d3f8af72d6145285329c457220ad43d092f7e1f
Takashi Tokumoto <t_tokumoto@yumemi.co.jp>
2021-04-27 14:01:45 +0900

    varが残っていたので削除

--- src/extension.ts
+++ src/extension.ts
@@ -10,9 +10,9 @@ const decorateInner = (tagInfo: TagInfo, editor: vscode.TextEditor, src: string)
 	if (tagInfo.decChar !== undefined) {
 		tagInfo.decChar.decorator.dispose();
 	}
-	var regex = new RegExp(`${commentSetting.startRegExp || commentSetting.start}|${commentSetting.endRegExp || commentSetting.end}|<(?:/|)${tagInfo.tagName}(?:$|(?:| (?:.*?)[^-?%$])(?<!=)>)`, 'gm');
+	const regex = new RegExp(`${commentSetting.startRegExp || commentSetting.start}|${commentSetting.endRegExp || commentSetting.end}|<(?:/|)${tagInfo.tagName}(?:$|(?:| (?:.*?)[^-?%$])(?<!=)>)`, 'gm');
 	let match: RegExpExecArray | null;
-	var inComment = false;
+	let inComment = false;
 	tagInfo.decChar = {
 		chars: [],
 		decorator: vscode.window.createTextEditorDecorationType({
@@ -20,7 +20,7 @@ const decorateInner = (tagInfo: TagInfo, editor: vscode.TextEditor, src: string)
 		})
 	};
 	while (match = regex.exec(src)) {
-		var splited = match[0].split(/[{}"]/);
+		const splited = match[0].split(/[{}"]/);
 		//コメントだったら飛ばす
 		if (match[0] === commentSetting.start) {
 			// コメント開始
@@ -40,17 +40,17 @@ const decorateInner = (tagInfo: TagInfo, editor: vscode.TextEditor, src: string)
 			splited.forEach(function (single, i) {
 				//偶数だったら
 				if (i % 2 === 0 && match !== null && tagInfo.decChar !== undefined) {
-					var startPos = editor.document.positionAt(match.index + singleLengths);
-					var endPos = editor.document.positionAt(match.index + singleLengths + single.length);
-					var range = new vscode.Range(startPos, endPos);
+					const startPos = editor.document.positionAt(match.index + singleLengths);
+					const endPos = editor.document.positionAt(match.index + singleLengths + single.length);
+					const range = new vscode.Range(startPos, endPos);
 					tagInfo.decChar.chars.push(range);
 				}
 				singleLengths += single.length + 1;
 			});
 		} else {
-			var startPos = editor.document.positionAt(match.index);
-			var endPos = editor.document.positionAt(match.index + match[0].length);
-			var range = new vscode.Range(startPos, endPos);
+			const startPos = editor.document.positionAt(match.index);
+			const endPos = editor.document.positionAt(match.index + match[0].length);
+			const range = new vscode.Range(startPos, endPos);
 			tagInfo.decChar.chars.push(range);
 		}
 	}
